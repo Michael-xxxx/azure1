d5d96252b3f1e7004fc49ef2670846d119bd4e1b
MichaelOnXPS <michael@xps.de>
2025-04-25 08:29:28 +0200

    qp

--- src/main/java/com/example/demo/controller/File.java
+++ src/main/java/com/example/demo/controller/File.java
@@ -1,13 +1,14 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class File {
     @GetMapping("/xxx")
-    public String home() {
-        return "someView";
+    public String home(@RequestParam String qP) {
+        return "someView: " + qP;
     }
 
 }
